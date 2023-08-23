93060dc5a35807b915504c46fcb0c9f5cf4ac1c6
sadjin25 <sadjin25@users.noreply.github.com>
2023-08-23 20:23:20 +0900

    wip: make flag
    DONE: board generation / w/h size, mine adjustable number / moving camera(moving with board size, but only x/y axis) / Cell structure(struct)

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+
+    [SerializeField] public Tile tileUnknown;
+    [SerializeField] public Tile tileEmpty;
+    [SerializeField] public Tile tileMine;
+    [SerializeField] public Tile tileExploded;
+    [SerializeField] public Tile tileFlag;
+    [SerializeField] public Tile[] tileNum;
+    void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    public void Draw(Cell[,] state)
+    {
+        int width = state.GetLength(0);
+        int height = state.GetLength(1);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = state[x, y];
+                tilemap.SetTile(cell.position, GetTile(cell));
+            }
+        }
+    }
+
+    Tile GetTile(Cell cell)
+    {
+        if (cell.isRevealed)
+        {
+            return GetRevealedTile(cell);
+        }
+        else if (cell.isFlagged)
+        {
+            return tileFlag;
+        }
+        else
+        {
+            return tileUnknown;
+        }
+    }
+
+    Tile GetRevealedTile(Cell cell)
+    {
+        switch (cell.type)
+        {
+            case Cell.Type.Empty:
+                return tileEmpty;
+            case Cell.Type.Mine:
+                return tileMine;
+            case Cell.Type.Number:
+                return GetNumberTile(cell);
+
+            default: return null;
+        }
+    }
+
+    Tile GetNumberTile(Cell cell)
+    {
+        if (cell.type != Cell.Type.Number) return null;
+
+        return tileNum[cell.number - 1];
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int width = 16;
+    [SerializeField] int height = 16;
+    [SerializeField] int mineCount = 4;
+
+    Board board;
+    Cell[,] state;
+
+    void Awake()
+    {
+        board = GetComponentInChildren<Board>();
+    }
+
+    void Start()
+    {
+        NewGame();
+    }
+
+    void NewGame()
+    {
+        state = new Cell[width, height];
+        // WARNING: Don't break this generation order.
+        GenerateCells();
+        GenerateMines();
+        GenerateNumbers();
+
+        Camera.main.transform.position = new Vector3(width / 2f, height / 2f, -10f);
+
+        board.Draw(state);
+    }
+
+    void GenerateCells()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cell cell = new Cell();
+                cell.position = new Vector3Int(x, y);
+                cell.type = Cell.Type.Invalid;
+                state[x, y] = cell;
+            }
+        }
+    }
+
+    void GenerateMines()
+    {
+        int mineNumToGenerate = mineCount > width * height ? width * height : mineCount;
+        for (int i = 0; i < mineNumToGenerate; i++)
+        {
+            int x = Random.Range(0, width);
+            int y = Random.Range(0, height);
+            if (state[x, y].type == Cell.Type.Mine)
+            {
+                i--;
+                continue;
+            }
+            state[x, y].type = Cell.Type.Mine;
+            state[x, y].isRevealed = true; //TESTETST
+        }
+    }
+
+    void GenerateNumbers()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (state[x, y].type != Cell.Type.Invalid) continue;
+
+                // NOTICE: Cell is value type(struct)
+                Cell cell = state[x, y];
+
+                cell.number = CountNumbers(state[x, y]);
+                if (cell.number > 0)
+                {
+                    cell.type = Cell.Type.Number;
+                }
+                else
+                {
+                    cell.type = Cell.Type.Empty;
+                }
+
+                cell.isRevealed = true; // TESTETST
+                state[x, y] = cell;
+            }
+        }
+    }
+
+    int CountNumbers(Cell cell)
+    {
+        if (cell.type != Cell.Type.Invalid) return -1;
+
+        int toReturn = 0;
+        for (int dx = -1; dx < 2; dx++)
+        {
+            for (int dy = -1; dy < 2; dy++)
+            {
+                if (dx == 0 && dy == 0) continue;
+
+                int x = cell.position.x + dx;
+                int y = cell.position.y + dy;
+
+                if (!IsValidPosition(x, y)) continue;
+                if (state[x, y].type == Cell.Type.Mine)
+                {
+                    toReturn++;
+                }
+            }
+        }
+        return toReturn;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            MakeFlag();
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            // Click();
+        }
+    }
+
+    void MakeFlag()
+    {
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 cellPos = board.tilemap.WorldToCell(worldPos);
+    }
+
+    Cell GetCell(int x, int y)
+    {
+        if (!IsValidPosition(x, y))
+        {
+            // return invalid type cell instance
+            return new Cell();
+        }
+        return state[x, y];
+    }
+
+    bool IsValidPosition(int x, int y)
+    {
+        return !(x < 0 || y < 0 || x >= width || y >= height);
+    }
+}
